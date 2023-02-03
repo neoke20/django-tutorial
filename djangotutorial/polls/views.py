25d973a61579e3874c5c1fdd8e5c1641487b145f
Kevin Konczak <konczakkevin@gmail.com>
2023-02-03 17:04:12 +0900

    Corrected view with correct url. Created a results.html to display votes

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -18,8 +18,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of the question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
@@ -28,7 +28,7 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(
-            request, 'polls/details.html', {
+            request, 'polls/detail.html', {
                 'question': question,
                 'error_message': "You did not select a choice.",
             })
