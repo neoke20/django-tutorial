0c2668227744ebcefb741bc9ff5fb9b676a9ee42
Kevin Konczak <konczakkevin@gmail.com>
2023-02-03 12:20:51 +0900

    Modified index view and created template index.html

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def details(request, question_id):
