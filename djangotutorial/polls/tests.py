90e3e385d8b5c483dc971991427c75cd699923c3
Kevin Konczak <konczakkevin@gmail.com>
2023-02-06 12:35:32 +0900

    Added test to make sure past questions are showing in view

--- djangotutorial/polls/tests.py
+++ djangotutorial/polls/tests.py
@@ -82,3 +82,10 @@ class QuestionDetailViewTests(TestCase):
         url = reverse('polls:detail', args=(future_question.id, ))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question(question_text="Past question.",
+                                        days=-5)
+        url = reverse('polls:detail', args=(past_question.id, ))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
