d0aedae2ed76da9554b6f2639916427fd5e8695b
Kevin Konczak <konczakkevin@gmail.com>
2023-02-06 13:33:57 +0900

    Moved Choice to be on the same admin page as Question

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -3,13 +3,18 @@ from .models import Choice, Question
 
 
 # Register your models here.
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [(None, {
         'fields': ['question_text']
     }), ('Date Information', {
         'fields': ['pub_date']
     })]
+    inlines = [ChoiceInLine]
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
